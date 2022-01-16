8d03f5701ecbbf9aa111c11a8c5f6e1a1adfef50
Henrique Costa <henriquesoarescosta2000@gmail.com>
2022-01-16 00:43:34 -0300

    Criação do máquina de estados e da função para ler o teclado

--- Simulacao/Simulacao Final/main.c
+++ Simulacao/Simulacao Final/main.c
@@ -1,14 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void limpa_buffer() {
-    int caracter=0;
-    do {
-    caracter = fgetc(stdin);
-    } while (caracter != '\n' && caracter!=EOF);
-    if(caracter==EOF)clearerr(stdin);
-}
-
 typedef struct
 {
     unsigned int indentificador;
@@ -19,7 +11,7 @@ typedef struct
 }Filme;
 
 enum Estados{
-    Inicial=0,
+    MenuInicial=0,
     Adicionar=1,
     Editar=2,
     Remover=3,
@@ -28,8 +20,12 @@ enum Estados{
     Ler=6,
 };
 
+void limpa_buffer();
+int le_numero(void *numero,double ri, double rf, char t);
+
 int main(){
-    enum Estados opcao=5;
+    enum Estados opcao;
+    le_numero(&opcao,1,6,'i');
     switch(opcao){
         case 1:
             printf("opcao 1\n");
@@ -53,5 +49,59 @@ int main(){
             printf("Olhe as opcoes\n");
         break;
     }
+    return 0;
+}
+
+void limpa_buffer() {
+    char caracter=0;
+    do {
+    caracter = fgetc(stdin);
+    } while (caracter != '\n' && caracter!=EOF);
+    if(caracter==EOF)clearerr(stdin);
+}
+
+int le_numero(void *numero,double ri, double rf, char t){
+    int erro=-1;
+    do
+    {
+        switch (t)
+        {
+        case 'd':
+            erro=scanf("%lf",(double *) numero);
+            if(erro==1){
+                if( (*(double *) numero) >= ri &&
+                (*(double *) numero) <= rf ) continue;
+                else erro=0;
+            }
+            break;
+        case 'f':
+            erro=scanf("%f",(float *) numero);
+            if(erro==1){
+                if( (*(float *) numero) >= (float) ri &&
+                (*(float *) numero) <= (float) rf ) continue;
+                else erro=0;
+            }
+            break;
+        case 'i':
+            erro=scanf("%d",(int *) numero);
+            if(erro==1){
+                if( (*(int *) numero) >= (int) ri &&
+                (*(int *) numero) <= (int) rf ) continue;
+                else erro=0;
+            }
+            break;
+        default:
+            erro=scanf("%c",(char *) numero);
+            if(erro==1){
+                if( (*(char *) numero) >= (char) ri &&
+                (*(char *) numero) <= (char) rf ) continue;
+                else erro=0;
+            }
+            break;
+        }
+        limpa_buffer();
+        printf("\nErro Digite Novamente: ");
+    } while (erro<=0);
+    limpa_buffer();
     return 0;
 }
